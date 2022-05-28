c9b69a7275b04a7986df06e976266fb2832462d7
bahadirsonmez <sonmezbahad@gmail.com>
2022-05-28 22:04:55 +0300

    Create country list table view and bind it

--- countries/Country List/CountryListViewController.swift
+++ countries/Country List/CountryListViewController.swift
@@ -9,8 +9,43 @@ import UIKit
 
 class CountryListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Countries"
+        setup()
+    }
+    
+    private func setup() {
+        configureTableView()
+    }
+    
+    private func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .singleLine
+        tableView.reloadData()
     }
     
 }
+
+extension CountryListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
+
+extension CountryListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.selectionStyle = .none
+        cell.textLabel?.text = "Test \(indexPath.row)"
+        return cell
+    }
+}
+
