6ea07e52e2c9a72a863f2c5de2120b2739c32139
bahadirsonmez <sonmezbahad@gmail.com>
2022-05-30 00:53:08 +0300

    Create elements and bind it into controller

--- countries/Country Detail/CountryDetailViewController.swift
+++ countries/Country Detail/CountryDetailViewController.swift
@@ -9,8 +9,16 @@ import UIKit
 
 class CountryDetailViewController: UIViewController {
 
+    @IBOutlet weak var flagImageView: UIImageView!
+    @IBOutlet weak var currenciesLabel: UILabel!
+    @IBOutlet weak var languagesLabel: UILabel!
+    @IBOutlet weak var neighboursTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func closeButtonTapped(_ sender: UIButton) {
+        
+    }
 }
