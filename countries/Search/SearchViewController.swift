fe4ccb93d065e19da2a5993f24efd5b3bc61fc2b
bahadirsonmez <sonmezbahad@gmail.com>
2022-05-29 17:15:06 +0300

    Create search view controller

---
+++ countries/Search/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  countries
+//
+//  Created by Bahadır Kişisel on 29.05.2022.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
