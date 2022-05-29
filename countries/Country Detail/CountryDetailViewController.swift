7a42d7047a0e2311a0b1b713c2961d2ce4121282
bahadirsonmez <sonmezbahad@gmail.com>
2022-05-30 00:26:06 +0300

    Create country detail view controller

---
+++ countries/Country Detail/CountryDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CountryDetailViewController.swift
+//  countries
+//
+//  Created by Bahadır Kişisel on 30.05.2022.
+//
+
+import UIKit
+
+class CountryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
