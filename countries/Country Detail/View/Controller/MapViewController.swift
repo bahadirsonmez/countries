adfcf5734269ee3fec80e23507a0a83aba94d008
bahadirsonmez <sonmezbahad@gmail.com>
2022-05-30 22:01:01 +0300

    Create MapViewController

--- countries/Country Detail/View/Controller/MapViewController.swift
+++ countries/Country Detail/View/Controller/MapViewController.swift
@@ -11,15 +11,48 @@ import MapKit
 class MapViewController: BaseViewController {
 
     @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    @IBOutlet weak var closeButton: UIBarButtonItem!
     
+    private let initialLocation: CLLocation
+    private let area: CLLocationDistance
+
+    init(location: CLLocation, area: CLLocationDistance) {
+        self.initialLocation = location
+        self.area = area
+        super.init(nibName: nil, bundle: nil)
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
+        mapView.centerToLocation(initialLocation, regionRadius: area)
     }
     
-    @IBAction func closeButtonTapped(_ sender: UIButton) {
+    private func configure() {
+        mapView.showsCompass = false
     }
-    
 
+    @IBAction func closeButtonTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
+
+private extension MKMapView {
+  func centerToLocation(
+    _ location: CLLocation,
+    regionRadius: CLLocationDistance = 100000
+  ) {
+    let coordinateRegion = MKCoordinateRegion(
+      center: location.coordinate,
+      latitudinalMeters: regionRadius,
+      longitudinalMeters: regionRadius)
+    setRegion(coordinateRegion, animated: true)
+  }
+}
+
